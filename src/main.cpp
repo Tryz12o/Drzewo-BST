7fb929e0a6bb93ab31f48a6c24c65f204b969606
Tryz12o <volodiaromaniak2@gmail.com>
2025-11-16 22:08:57 +0000

    Pozostale funkcji

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@ int main() {
         cout << "\n==== MENU ====\n";
         cout << "1. Dodaj element\n";
         cout << "2. Usuń element\n";
+        cout << "3. Usuń całe drzewo\n";
+        cout << "4. Szukaj drogi do elementu\n";
+        cout << "5. Wyświetl drzewo graficznie (preorder/inorder/postorder)\n";
+        cout << "6. Zapisz drzewo do pliku\n";
         cout << "0. Wyjście\n";
         cout << "Wybierz: ";
         cin >> choice;
@@ -24,6 +28,30 @@ int main() {
                 cout << "Podaj wartość: ";
                 cin >> val;
                 tree.remove(val);
+                break;
+
+            case 3:
+                tree.clear();
+                cout << "Drzewo usunięte.\n";
+                break;
+
+            case 4:
+                cout << "Podaj wartość: ";
+                cin >> val;
+                tree.findPath(val);
+                break;
+
+            case 5:
+                cout << "Wybierz sposób wyświetlania drzewa:\n";
+                cout << "1. Preorder\n2. Inorder\n3. Postorder\nWybierz: ";
+                cin >> val;
+                tree.displayGraphical(val);
+                break;
+
+            case 6:
+                cout << "Nazwa pliku: ";
+                cin >> filename;
+                tree.saveToFile(filename);
                 break;
 
             case 0:
